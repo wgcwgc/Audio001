c611314f98dc924641c3456612854c678b29127a
king-sunshine-spring <1348066599@qq.com>
2017-02-14 17:30:17 +0800

    @wgcwgc  Audio01
    @wgcwgc  Audio01
    The last update
    @ author  wgcwgc
    @ time 2017.02.14
    @ address runcom
    @ config intel(R) Core (TM) i5 - 2310 cpu @ 2.90GHz 3.20GHz
    8.00GB(7.89GB可用) 64bit
    windows 7 旗舰版
    @ detail mark

--- src/com/runcom/wgcwgc/recordServer/Recorder.java
+++ src/com/runcom/wgcwgc/recordServer/Recorder.java
@@ -12,7 +12,6 @@ import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
